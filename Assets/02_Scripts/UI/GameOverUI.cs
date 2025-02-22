35ef88ff20295b38216734e93196d4d9f147be4b
yukgeunmu <36117247+yukgeunmu@users.noreply.github.com>
2025-02-22 16:11:49 +0900

    [feat] GameOverUI 구현

--- Assets/02_Scripts/UI/GameOverUI.cs
+++ Assets/02_Scripts/UI/GameOverUI.cs
@@ -9,10 +9,15 @@ public class GameOverUI : BaseUI
 
     [SerializeField] private Button restartButton;
     [SerializeField] private Button exitButton;
+    [SerializeField] private Text scoreText;
+    [SerializeField] private Text bestScoreText;
 
     public override void Init(UIManager uIManager)
     {
         base.Init(uIManager);
+
+        scoreText = transform.Find("Score").GetComponent<Text>();
+        bestScoreText = transform.Find("BestScore").GetComponent<Text>();
 
         restartButton.onClick.AddListener(OnClickRestartButton);
         exitButton.onClick.AddListener(OnClickExitButton);
@@ -26,6 +31,12 @@ public class GameOverUI : BaseUI
     public void OnClickExitButton()
     {
         Application.Quit();
+    }
+
+    public void SetResultGameOverScore(int score, int bestScore)
+    {
+        scoreText.text = score.ToString();
+        bestScoreText.text = score.ToString();
     }
 
     protected override UIState GetUIstate()
