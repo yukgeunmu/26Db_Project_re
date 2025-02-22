db18ded4ed8c2dbca86409e39849dde2c1afd04d
yukgeunmu <36117247+yukgeunmu@users.noreply.github.com>
2025-02-22 15:57:07 +0900

    [feat] GameUI 구현

--- Assets/02_Scripts/UI/GameUI.cs
+++ Assets/02_Scripts/UI/GameUI.cs
@@ -7,17 +7,21 @@ public class GameUI : BaseUI
 {
 
     [SerializeField] private Slider hpSlider;
-    [SerializeField] private Text coin;
+    [SerializeField] private Text coinText;
     [SerializeField] private Text scoreText;
     [SerializeField] private Text bestScoreText;
+    [SerializeField] private Button jumpButton;
+    [SerializeField] private Button slideButton; 
 
     public override void Init(UIManager uIManager)
     {
         base.Init(uIManager);
         hpSlider = transform.Find("Slider").GetComponent<Slider>();
-        coin = transform.Find("Coin").GetComponent<Text>();
-        scoreText = transform.Find("Score").GetComponent<Text>();
-        bestScoreText = transform.Find("BestScore").GetComponent<Text>();
+        coinText = transform.Find("CointText").GetComponent<Text>();
+        scoreText = transform.Find("ScoreText").GetComponent<Text>();
+        bestScoreText = transform.Find("BestScoreText").GetComponent<Text>();
+        jumpButton.onClick.AddListener(OnClickJumpButton);
+        slideButton.onClick.AddListener(OnClickSlideButton);
     }
 
     private void Start()
@@ -39,6 +43,17 @@ public class GameUI : BaseUI
     public void UpdateScore(int currentScore)
     {
         scoreText.text = currentScore.ToString();
+    }
+
+
+    public void OnClickJumpButton()
+    {
+
+    }
+
+    public void OnClickSlideButton()
+    {
+
     }
 
 
