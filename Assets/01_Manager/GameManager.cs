0092ffde20eb3753f6840ebdfce49f67f89baa66
yukgeunmu <36117247+yukgeunmu@users.noreply.github.com>
2025-02-22 16:17:05 +0900

    [feat] GameManager에 싱글톤 구현 및 UImnager 작성

--- Assets/01_Manager/GameManager.cs
+++ Assets/01_Manager/GameManager.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager Instance;
+
+    private UIManager uiManager;
+
+    private void Awake()
     {
-        
+        uiManager = FindObjectOfType<UIManager>();
+
+        if(Instance == null)
+        {
+            Instance = this;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartGame()
     {
-        
+        uiManager.SetPlayGame();
     }
+
+    public void GameOver()
+    {
+        uiManager.SetGameOver();
+    }
+
 }
