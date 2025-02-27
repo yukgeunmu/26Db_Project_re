96d54e1e70d3768c17535299dd4dfaeaeadf9974
DasaemKim <kds000203@gmail.com>
2025-02-27 17:42:09 +0900

    (feat)기능수정

---
+++ Assets/02_Scripts/Controller/ExitButtonController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitButtonController : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Debug.Log("°×Á¾·á");
+        Application.Quit();
+    }
+}
---
+++ Assets/02_Scripts/Controller/GameStartButtonController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStartButtonController : MonoBehaviour
+{
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+}
